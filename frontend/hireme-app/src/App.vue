c846ee0ce9248ca3b6a6620c93c124c1e221491a
Talig81 <rotundix@gmail.com>
2020-09-15 12:18:50 +0100

    starting

---
+++ frontend/hireme-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Avatar/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Avatar from './components/Avatar';
+
+export default {
+  name: 'App',
+
+  components: {
+    Avatar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ frontend/hireme-app/src/components/Avatar.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container
+  fluid
+  fill-height
+  class="containers">
+    <v-row align="start">
+      
+      <v-col cols="12" class="cenas1">
+        <v-card height="400px" shaped="true">
+          <v-card-title class="justify-center"> Hire Me! </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Avatar',
+
+    data: () => ({
+    }),
+  }
+</script>
+
+<style scoped>
+.cenas {
+  background: red
+}
+.cenas1 {
+  background: green
+}
+.cenas2 {
+  background: lightblue
+}
+.rounded-card{
+    border-radius:50px;
+}
+.containers {
+  background: lightgrey
+}
+.justify-center{
+  font-family: "Lucida Console", Courier, monospace;
+  font-size: 58px;
+}
+</style>
